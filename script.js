2e72214968cdade7f8db28bb16cd09e82ccae721
jleynes <38470756+jleynes@users.noreply.github.com>
2021-08-21 17:10:20 -0700

    Updated files

--- script.js
+++ script.js
@@ -8,8 +8,31 @@ function makeGrid(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
     let grid = document.createElement("div");
-    container.appendChild(grid).className = "grid-item"
+    container.appendChild(grid).className = "grid-item";
+    grid.addEventListener("mouseover", addColor, false);
   }
 };
+
+function addColor() {
+  const square = document.querySelectorAll('.grid-item');
+
+  square.forEach((square) => {
+    square.addEventListener("mouseover", function(e) {
+      e.target.style.backgroundColor = '#9999ff';
+    });
+  });
+}
+
+function clearColor() {
+  const gridItems = document.querySelectorAll('#grid-item');
+
+  gridItems.forEach((item) => {
+    item.style.backgroundColor = '#ffffff';
+  });
+}
+
+// Event listeners
+const eraseButton = document.getElementById('clearButton');
+eraseButton.addEventListener("click", clearColor);
 
 makeGrid(16, 16);
